ed4d1a2685ea70c527c69432185a8ad99a4cd611
RodgersMwangi <rdgrsmwangi@gmail.com>
2026-02-11 12:18:22 -0500

    updating fileCreator.py

--- fileCreator.py
+++ fileCreator.py
@@ -3,54 +3,58 @@ import subprocess
 from pathlib import Path
 
 # FORGET THE CODE, WE ARE ONLY PRINTING OUR NAMES BELOW HERE TO DEMO GIT.
-print("Amara Nyanzi")
+#print("Amara Nyanzi")
 
 
-print("Eve Candy")
+#print("Eve Candy")
 
 
-print("Mathew Kasanga")
+#print("Mathew Kasanga")
 
 
-print("Steve Muturi")   #<-- Add name here
+#print("Steve Muturi")   #<-- Add name here
 
-print("Dennis Musyimi")
+#print("Dennis Musyimi")
 
 
 # -------------------------
 # Create the first directory
 # -------------------------
 
-# directoryPath = Path("dirOne")
+directoryPath = Path("dirOne")
 
-# if directoryPath.exists():
-#     print("DirOne already exists. Deleting any files within...")
-#     subprocess.run("find dirOne -mindepth 1 -delete", shell=True)
-# else:
-#     directoryPath.mkdir()
-#     print("DirOne has been created")
+if directoryPath.exists():
+     print("DirOne already exists. Deleting any files within...")
+     subprocess.run("find dirOne -mindepth 1 -delete", shell=True)
+else:
+     directoryPath.mkdir()
+     print("DirOne has been created")
 
 # # -------------------------
 # # Create the second directory
 # # -------------------------
 
-# directoryPath = Path("dirTwo")
+directoryPath = Path("dirTwo")
 
-# if directoryPath.exists():
-#     print("DirTwo already exists. Deleting any files within...")
-#     subprocess.run("find dirTwo -mindepth 1 -delete", shell=True)
-# else:
-#     directoryPath.mkdir()
-#     print("DirTwo has been created")
+if directoryPath.exists():
+     print("DirTwo already exists. Deleting any files within...")
+     subprocess.run("find dirTwo -mindepth 1 -delete", shell=True)
+else:
+     directoryPath.mkdir()
+     print("DirTwo has been created")
 
 # # -------------------------
 # # Create text files
 # # -------------------------
 
-# name = "Sharon Opudo"
-# filename = "Sharon.txt"
+name = "Sharon Opudo"
+filename = "Sharon.txt"
 
-# subprocess.run(f"echo '{name}' > dirOne/{filename}", shell=True)
-# subprocess.run(f"echo '{name}' > dirTwo/{filename}", shell=True)
+subprocess.run(f"echo '{name}' > dirOne/{filename}", shell=True)
+subprocess.run(f"echo '{name}' > dirTwo/{filename}", shell=True)
 
-# print("txt files created")
+subprocess.run("echo 'Rodgers Mwangi' > dirOne/Rodgers.txt", shell=True)
+subprocess.run("echo 'Rodgers Mwangi' > dirTwo/Rodgers.txt", shell=True)
+
+
+print("txt files created")
