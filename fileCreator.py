388e3aa058d009b51f1bfcde29d5474fc949d0cb
anzieri <amaranyanzi1@gmail.com>
2026-02-26 09:46:19 +0300

    print statement to demo Jenkins

--- fileCreator.py
+++ fileCreator.py
@@ -7,6 +7,7 @@ print("Get Git")
 
 print("Welcome to the file creator script.")
 print("Testing testing Jenkins integration!")
+print("New Change to demo")
 # FORGET THE CODE, WE ARE ONLY PRINTING OUR NAMES BELOW HERE TO DEMO GIT.
 #print("Amara Nyanzi")
 
